3d127271f724457daf7d42f0f1f6f0652d1d6046
Dean Nellessen <51295582+Djassie18698@users.noreply.github.com>
2022-09-13 13:57:38 +0200

    4.4 update

---
+++ Scripting/Python/Blok 1/Week 4/4.4 RAMplotter.py
@@ -0,0 +1,8 @@
+import psutil
+import time
+#from datetime import datetime, timedelta
+#time = input("How long do you want to check the RAM (in seconds) ")
+while True:
+    plotter = ("#" * int(psutil.virtual_memory().percent))
+    time.sleep(0.5)
+    print (plotter)
