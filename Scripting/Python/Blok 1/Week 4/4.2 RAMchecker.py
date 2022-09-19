78102527b3bc95426445099b32c3f5df0f2e30c4
Dean Nellessen <51295582+Djassie18698@users.noreply.github.com>
2022-09-19 14:29:08 +0200

    Update in some scripts

--- Scripting/Python/Blok 1/Week 4/4.2 RAMchecker.py
+++ Scripting/Python/Blok 1/Week 4/4.2 RAMchecker.py
@@ -1,5 +1,12 @@
 from ast import If
 from collections import deque
 import psutil
-if psutil.virtual_memory().percent > 35:
+import time
+
+while psutil.virtual_memory().percent > 35:
+   time.sleep(0.5)
    print("RAM usage is above 35%")
+else:
+   time.sleep(0.5)
+   print("Ram usage is below 35%")
+   
